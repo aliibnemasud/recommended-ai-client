818278a72074b6c180ccb12f3fbd4335da188901
aliibnemasud <mdmasudrony@gmail.com>
2022-10-17 16:22:21 +0600

    router added

---
+++ src/componants/Pages/Recommanded/RecommandedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecommandedPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RecommandedPage;
---
+++ src/componants/questions/Questions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Questions = () => {
+    return (
+        <div>
+            <h1>Which type of movie you like ?</h1>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Questions;
